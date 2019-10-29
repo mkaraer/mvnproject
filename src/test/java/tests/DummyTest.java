dd50a5612eab235be2de50fbfb0d7c93fff359e9
Ahmed Birol <mkaraer44@hotmail.com>
2019-10-28 21:18:07 -0400

    firstCommit

---
+++ src/test/java/tests/DummyTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DummyTest {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().clearPreferences();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.google.com");
+      //  driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("Metin Karaer",Keys.ENTER);
+        driver.findElement(By.xpath("//*[(text()='Metin Karaer - Mozambique | Professional Profile | LinkedIn')]")).click();
+
+    }
+
+}
